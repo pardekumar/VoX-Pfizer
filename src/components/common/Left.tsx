ec89ded9b6f0b5f259093d565a529b17ea386b07
Pardeep Kumar <pardekumar@deloitte.com>
2023-06-21 23:55:14 +0530

    Updated the base code

--- src/components/common/Left.tsx
+++ src/components/common/Left.tsx
@@ -14,12 +14,17 @@ import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import { styled, useTheme } from "@mui/material/styles";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
+import TextField from "@mui/material/TextField";
+import DoneIcon from "@mui/icons-material/Done";
+import CloseIcon from "@mui/icons-material/Close";
 
 import "./Left.css";
 
 export default function Response(props: any) {
   const { open, handleDrawerOpen, handleDrawerClose } = props;
   const [selected, setSelected] = React.useState([-1, -1]);
+  const [editmode, setEditmode] = React.useState([-1, -1]);
+  const [deletemode, setDeletemode] = React.useState([-1, -1]);
 
   const leftMenuData = {
     sections: [
@@ -61,10 +66,16 @@ export default function Response(props: any) {
     ],
   };
 
-  const menuItems = () => {
+  const menuItems = (i: any, ci: any) => {
     return (
       <div className="item-menu-icons">
-        <div className="item-menu-icon">
+        <div
+          className="item-menu-icon"
+          onClick={() => {
+            setEditmode([i, ci]);
+          }}
+        >
+          {/* edit */}
           <svg
             stroke="currentColor"
             fill="none"
@@ -99,7 +110,12 @@ export default function Response(props: any) {
             <line x1="12" y1="2" x2="12" y2="15"></line>
           </svg>
         </div>
-        <div className="item-menu-icon">
+        <div
+          className="item-menu-icon"
+          onClick={() => {
+            setDeletemode([i, ci]);
+          }}
+        >
           <svg
             stroke="currentColor"
             fill="none"
@@ -117,6 +133,53 @@ export default function Response(props: any) {
             <line x1="10" y1="11" x2="10" y2="17"></line>
             <line x1="14" y1="11" x2="14" y2="17"></line>
           </svg>
+        </div>
+      </div>
+    );
+  };
+
+  const editMenuItems = (i: any, ci: any) => {
+    return (
+      <div className="item-menu-icons">
+        <div className="item-menu-icon">
+          <DoneIcon
+            style={{ fontSize: "20px" }}
+            onClick={() => {
+              // update session title
+            }}
+          />
+        </div>
+        <div className="item-menu-icon">
+          <CloseIcon
+            style={{ fontSize: "20px" }}
+            onClick={() => {
+              setEditmode([-1, -1]);
+            }}
+          />
+        </div>
+      </div>
+    );
+  };
+
+  const deleteMenuItems = (i: any, ci: any) => {
+    return (
+      <div className="item-menu-icons">
+        <div className="item-menu-icon">
+          <DoneIcon
+            style={{ fontSize: "20px" }}
+            onClick={() => {
+              // update session title
+              setDeletemode([-1, -1]);
+            }}
+          />
+        </div>
+        <div className="item-menu-icon">
+          <CloseIcon
+            style={{ fontSize: "20px" }}
+            onClick={() => {
+              setDeletemode([-1, -1]);
+            }}
+          />
         </div>
       </div>
     );
@@ -221,18 +284,60 @@ export default function Response(props: any) {
                               <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
                             </svg>
                           </ListItemIcon>
-                          <ListItemText
-                            primary={citem.title}
-                            className="list-item-text"
-                          />
-                          <div className="item-menu">
-                            {selected[0] === i && selected[1] === ci ? (
-                              <>{menuItems()}</>
-                            ) : (
-                              <></>
-                            )}
-                            &nbsp;
-                          </div>
+                          {editmode[0] === i && editmode[1] === ci ? (
+                            <>
+                              <TextField
+                                className="input-field"
+                                defaultValue="Hello World"
+                                variant="outlined"
+                                size="small"
+                                sx={{
+                                  width: "60%",
+                                  marginLeft: "5px",
+                                }}
+                                autoFocus={true}
+                                onBlur={() => {
+                                  setEditmode([-1, -1]);
+                                }}
+                              />
+
+                              <div className="item-menu-edit">
+                                {selected[0] === i && selected[1] === ci ? (
+                                  <>{editMenuItems(i, ci)}</>
+                                ) : (
+                                  <></>
+                                )}
+                              </div>
+                            </>
+                          ) : (
+                            <>
+                              {deletemode[0] === i && deletemode[1] === ci ? (
+                                <>
+                                  <ListItemText
+                                    primary={citem.title}
+                                    className="list-item-text"
+                                  />
+                                  <div className="item-menu">
+                                    {deleteMenuItems(i, ci)}
+                                  </div>
+                                </>
+                              ) : (
+                                <>
+                                  <ListItemText
+                                    primary={citem.title}
+                                    className="list-item-text"
+                                  />
+                                  <div className="item-menu">
+                                    {selected[0] === i && selected[1] === ci ? (
+                                      <>{menuItems(i, ci)}</>
+                                    ) : (
+                                      <></>
+                                    )}
+                                  </div>
+                                </>
+                              )}
+                            </>
+                          )}
                         </ListItemButton>
                       </ListItem>
                     ))}
